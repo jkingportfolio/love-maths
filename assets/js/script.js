a614ab54efcf45fa6ab4589414ec069a25783074
jkingportfolio <mycodejourney3@gmail.com>
2022-06-06 18:02:54 +0000

    corrected spellin mistake in function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,19 @@
 
-document.addEventListener("DOMContentLoaded", function(){
+document.addEventListener("DOMContentLoaded", function() {
     let buttons = document.getElementsByTagName("button");
 
     for (let button of buttons) {
-        button.addEventListener("click", fucntion() {
+        button.addEventListener("click", function() {
             if (this.getAttribute(data-type) === "submit") {
                 alert("You clicked submit");
-            }
-            else {
+            } else {
                 let.gameType = this.getAttribute("data-type");
                 alert(`You clicked ${gameType}`);
             }
-        })
+        });
     }
-})
+});
+
 
 function runGame() {
 
